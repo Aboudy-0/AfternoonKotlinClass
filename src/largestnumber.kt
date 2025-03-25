756fcac498631b4ae631c60e2bd7ecd58f7c9563
Aboudy-0 <aboudshiek17@gmail.com>
2025-03-25 15:39:41 +0300

    third commit

---
+++ src/largestnumber.kt
@@ -0,0 +1,29 @@
+import java.util.Scanner
+fun main() {
+    var read = Scanner(System.`in`)
+
+    println("Enter first number :")
+    var a = read.nextInt()
+
+    println("Enter second number :")
+    var b = read.nextInt()
+
+    println("Enter third number :")
+    var c = read.nextInt()
+
+    if(a >= b && a >= c){
+        println("$a is the largest number")
+    }
+
+    else if (b >= a && b >= c){
+        println("$b is the largest number")
+    }
+
+    else{
+        println("$c is the largest number")
+    }
+
+
+
+
+}
---
+++ src/loops.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //While loop
+    var number = 20
+    while (number <= 25){
+        println("Number : $number ")
+        number ++
+    }
+    var num = 5
+    while (num >= 1){
+        println("Number : $num")
+        num --
+    }
+
+    //Do.. While loop
+    var x = 30
+    do {
+        println("Number is $x")
+        x ++
+    }
+        while (x <= 35)
+
+}
