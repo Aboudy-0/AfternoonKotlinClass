dee3c57db428a845dd67f044c7569eb7cc62747b
Aboudy-0 <aboudshiek17@gmail.com>
2025-03-25 15:53:56 +0300

    third commit

--- src/loops.kt
+++ src/loops.kt
@@ -19,4 +19,27 @@ fun main() {
     }
         while (x <= 35)
 
+    //For loop
+    for (a in 100..105)
+        println("Number is $a")
+
+    for (letter in 'a'..'d')
+        println("My letter is $letter")
+
+    //Break
+    for (b in 70 .. 75){
+        if (b == 73){
+            break
+        }
+        println("Number is $b")
+    }
+
+    //Continue
+    for (character in 'd'..'h'){
+        if (character == 'f'){
+            continue
+        }
+        println("Character is $character")
+    }
+
 }
